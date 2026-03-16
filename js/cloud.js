6ac427377816a3c345bef882e7d362c8d34e0c09
Erika400 <eraipacs@gmail.com>
2026-03-16 16:13:08 +0100

    cloud.js

--- js/cloud.js
+++ js/cloud.js
@@ -1,19 +1,41 @@
 console.log("cloud.js betöltve");
 
-async function loadItemsCloud() {
+async function loadProductsCloud(username) {
+  console.log("Cloud products betöltés még nincs bekötve:", username);
   return [];
 }
 
-async function saveItemsCloud(items) {
-  console.log("Cloud items mentés később ide jön:", items);
+async function saveProductsCloud(username, products) {
+  console.log("Cloud products mentés még nincs bekötve:", username, products);
   return true;
 }
 
-async function loadShoppingCloud() {
+async function loadShoppingListCloud(username) {
+  console.log("Cloud shopping list betöltés még nincs bekötve:", username);
   return [];
 }
 
-async function saveShoppingCloud(list) {
-  console.log("Cloud shopping mentés később ide jön:", list);
+async function saveShoppingListCloud(username, list) {
+  console.log("Cloud shopping list mentés még nincs bekötve:", username, list);
+  return true;
+}
+
+async function loadKnownProductsCloud(username) {
+  console.log("Cloud known products betöltés még nincs bekötve:", username);
+  return {};
+}
+
+async function saveKnownProductsCloud(username, data) {
+  console.log("Cloud known products mentés még nincs bekötve:", username, data);
+  return true;
+}
+
+async function loadWasteHistoryCloud(username) {
+  console.log("Cloud waste history betöltés még nincs bekötve:", username);
+  return [];
+}
+
+async function saveWasteHistoryCloud(username, history) {
+  console.log("Cloud waste history mentés még nincs bekötve:", username, history);
   return true;
 }
