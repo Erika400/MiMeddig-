e1ffd926949cc26289136b3894e571d983ea4e1b
Erika400 <eraipacs@gmail.com>
2026-03-16 14:53:05 +0100

    cloud.js

--- js/cloud.js
+++ js/cloud.js
@@ -1,1 +1,19 @@
+// js/cloud.js
 
+console.log("cloud.js betöltve");
+
+async function loadProductsCloud() {
+  return [];
+}
+
+async function saveProductCloud(product) {
+  return true;
+}
+
+async function updateProductCloud(product) {
+  return true;
+}
+
+async function deleteProductCloud(productId) {
+  return true;
+}
